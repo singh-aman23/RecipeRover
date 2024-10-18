75307d2041802a2421ce5d62133cf7c1cf5c66a9
Aman <singh2004aman@gmail.com>
2024-10-18 12:29:01 +0530

    fixed minor bug

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
 import "./globals.css";
 import { Poppins } from "@next/font/google";
-import { AuthProvider } from "./provider";
-
+import { AuthProvider } from "./Provider";
 export const metadata = {
   title: "RecipeRover",
   description:
     "Discover delicious recipes from around the world! Share your own unique food creations and explore recipes from other passionate cooks.",
 };
 
-// Import and configure Poppins font
 const poppins = Poppins({
   weight: ["400", "600"],
   subsets: ["latin"],
